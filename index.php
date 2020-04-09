263791b94af953eab472f553ac38fc342f2b2de0
yo <yo@DESKTOP-RVOAHLO>
2020-04-09 14:00:40 -0400

    initial commit.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Mi pagina web</title>
+        <link href="css/estilo_index.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <header class="encabezados">
+            <h1>Bienvenidos a la pagina del cuadrado magico</h1>
+        </header>
+        <header id="encabezado2" class="encabezados">
+            <h2>Subtitulo</h2>
+        </header>
+    </body>
+</html>
